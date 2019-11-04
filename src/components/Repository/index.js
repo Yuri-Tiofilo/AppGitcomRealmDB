145776706eb2934f8865588a92f5409cda6d8798
oEstagiario <yusilva12345@gmail.com>
2019-11-04 10:27:54 -0300

    04/11/2019

--- src/components/Repository/index.js
+++ src/components/Repository/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 // import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {Container, Name, Description, Stats, Stat, StatCount} from './styles';
 
-import {Container} from './styles';
-
-export default function Repository() {
-  return <Container />;
+export default function Repository({data}) {
+  return (
+    <Container>
+      <Name>{data.name}</Name>
+      <Description>{data.description}</Description>
+      <Stats>
+        <Stat>
+          <Icon name="star" size={16} color="#333" />
+          <StatCount>{data.stars}</StatCount>
+        </Stat>
+        <Stat>
+          <Icon name="code-fork" size={16} color="#333" />
+          <StatCount>{data.stars}</StatCount>
+        </Stat>
+      </Stats>
+    </Container>
+  );
 }
